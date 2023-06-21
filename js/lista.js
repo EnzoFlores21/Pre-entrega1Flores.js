64ab22cbd59041ca0f225ce0cf973027a42f36a6
enzoflores <enzoflores15k@gmail.com>
2023-06-21 16:09:31 -0300

    Reeentrega

--- js/lista.js
+++ js/lista.js
@@ -1,26 +1,56 @@
-function marca(entrada){
-    return entrada = 'Marvel';
+let seguir = true;
+
+const personajes = () => {
+    while (seguir) {
+        let seleccion = prompt("Ingrese un numero para seleccionar un personaje");
+        switch (seleccion) {
+            case "1":
+                console.log("Iron Man");
+                caracteristicas("1")
+                seguir = false;
+                break;
+            case "2":
+                console.log("Hulk");
+                caracteristicas("2")
+                seguir = false;
+                break;
+            case "3":
+                console.log("Thor");
+                caracteristicas("3")
+                seguir = false;
+                break;
+            case "4":
+                console.log("Mujer Maravilla");
+                caracteristicas("4")
+                seguir = false;
+                break;
+            default:
+                console.log("Seleccione un personaje correcto");
+                seguir = true;
+        }
+    }
 }
 
-let entrada =  prompt("Selecciona un Personaje");
-while (entrada != 'ESC') {
-    switch (entrada) {
-        case "1":
-            alert("Iron Man");
+const caracteristicas = (personaje) =>{
+    let elegirPersonaje = prompt("Ingrese el nombre del Personaje para mostrar habilidad");
+    while (personaje == "1" || personaje == "2" || personaje == "3" || personaje == "4") {
+        if (elegirPersonaje == "Iron Man") {
+            console.log("Habilidad: Inteligencia");
             break;
-        case "2":
-            alert("Hulk");
+        } else if (elegirPersonaje == "Hulk") {
+            console.log("Habilidad: Fuerza");
             break;
-        case "3":
-            alert("Mujer Maravilla");
+        } else if (elegirPersonaje == "Thor") {
+            console.log("Habilidad: Fuerza");
             break;
-        case "4":
-            alert("Spiderman");
+        } else if (elegirPersonaje == "Mujer Maravilla") {
+            console.log("Habilidad: Velocidad");
             break;
-        default:
-            alert("Elige entre 4 personajes");
+        } else {
+            console.log("Ingrese Personaje un correcto");
             break;
+        }
     }
-    alert(marca(entrada));
-    entrada = prompt("Selecciona un nuevo personaje");
 }
+
+personajes();
